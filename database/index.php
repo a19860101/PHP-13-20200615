22d03d480a5abd6283b2dd4d24050446dadb8a40
Zac <a19860101@gmail.com>
2020-06-24 21:58:06 +0800

    20200624

--- database/index.php
+++ database/index.php
@@ -1,6 +1,22 @@
 <?php
     require_once("conn.php");
-?>
+    $sql = "SELECT * FROM students";
+    $result = mysqli_query($conn,"SELECT * FROM students");
+    $row = mysqli_fetch_assoc($result);
+
+    // $row = mysqli_fetch_row($result);
+    // $row = mysqli_fetch_array($result);
+    echo $row["user"];
+    echo "<br>";
+    echo $row["mail"];
+    echo "<br>";
+    echo $row["gender"];
+    echo "<br>";
+    echo $row["edu"];
+    echo "<br>";
+    echo $row["create_at"];
+    echo "<br>";
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
