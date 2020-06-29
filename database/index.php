3d4aeffcf303e0a73f44437132fe209021cd5646
Zac <a19860101@gmail.com>
2020-06-29 19:28:01 +0800

    20200629

--- database/index.php
+++ database/index.php
@@ -14,17 +14,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table,td,th {
+            border-collapse: collapse;
+            border: 1px solid #666;
+            padding: 5px;
+        }
+    </style>
 </head>
 <body>
 <h1>hello php</h1>
+<table>
+    <tr>
+        <th>#</th>
+        <th>姓名</th>
+        <th>MAIL</th>
+        <th>性別</th>
+        <th>學歷</th>
+    </tr>
+    <?php while($row = mysqli_fetch_assoc($result)){ ?> 
+    <tr>
+        <td><?php echo $row["id"];?></td>
+        <td><?php echo $row["user"];?></td>
+        <td><?php echo $row["mail"];?></td>
+        <td><?php echo $row["gender"];?></td>
+        <td><?php echo $row["edu"];?></td>
+    </tr>
+    <?php } ?>
+</table>
+
+
+
+
+
 <?php
-    while($row = mysqli_fetch_assoc($result)){
-        echo $row["user"];
-        echo $row["mail"];
-        echo $row["gender"];
-        echo $row["edu"];
-        echo "<hr>";
-    }
+    // while($row = mysqli_fetch_assoc($result)){
+    //     echo "<tr>";
+    //     echo "<td>".$row["id"]."</td>";
+    //     echo "<td>".$row["user"]."</td>";
+    //     echo "<td>".$row["mail"]."</td>";
+    //     echo "<td>".$row["gender"]."</td>";
+    //     echo "<td>".$row["edu"]."</td>";
+    //     echo "</tr>";
+    // }
 ?>
+    <!-- </table> -->
 </body>
 </html>
