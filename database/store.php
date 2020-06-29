550295b9af594ec489b06c7f63a7ff213d77a54c
Zac <a19860101@gmail.com>
2020-06-29 20:05:56 +0800

    20200629

--- database/store.php
+++ database/store.php
@@ -7,4 +7,8 @@
     $skill = implode(",",$_POST["skill"]);
     $comment = $_POST["comment"];
 
-    echo $user,$mail,$gender,$edu,$skill,$comment;
+    // echo $user,$mail,$gender,$edu,$skill,$comment;
+    $sql = "INSERT INTO students (user,mail,gender,edu,skill,comment,create_at)
+            VALUES('$user','$mail','$gender','$edu','$skill','$comment',NOW())";
+    mysqli_query($conn,$sql);
+    header("location:index.php");
