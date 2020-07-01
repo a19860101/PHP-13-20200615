93ff559cff2bfb938e7facabd55a700426952038
Zac <a19860101@gmail.com>
2020-07-01 19:46:54 +0800

    20200701

--- pdo/index.php
+++ pdo/index.php
@@ -5,7 +5,10 @@
         #預備陳述式
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
-        
+        $rows = array();
+        while($row = $stmt->fetch()){
+           $rows[] = $row;
+        }
     }catch(PDOException $e){
         echo $e->getMessage();
     }
@@ -16,12 +19,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table,td,th {
+            border-collapse: collapse;
+            border: 1px solid #666;
+            padding: 5px;
+        }
+    </style>
 </head>
 <body>
-    <?php
-        while($row = $stmt->fetch()){
-            echo $row["user"];
-        }
-    ?>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>姓名</th>
+            <th>MAIL</th>
+            <th>性別</th>
+            <th>學歷</th>
+            <th>動作</th>
+        </tr>
+        <?php foreach($rows as $r){ ?>
+        <tr>
+            <td><?php echo $r["id"];?></td>
+            <td><?php echo $r["user"];?></td>
+            <td><?php echo $r["mail"];?></td>
+            <td><?php echo $r["gender"];?></td>
+            <td><?php echo $r["edu"];?></td>
+            <td>
+                <a href="detail.php?id=<?php echo $r["id"];?>">檢視</a>
+                <a href="edit.php?id=<?php echo $r["id"];?>">編輯</a>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
