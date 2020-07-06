905f57fd014e598b536cd80d05b0def43be7dfd6
Zac <a19860101@gmail.com>
2020-07-06 20:18:29 +0800

    20200706

--- gd/img.php
+++ gd/img.php
@@ -6,8 +6,13 @@
     $canvas_h = imagesy($canvas);
     // echo $canvas_w,$canvas_h;
 
-    $new_w = $canvas_w / 2;
-    $new_h = $canvas_h / 2;
+    $new_w = $canvas_w / 3;
+    $new_h = $canvas_h / 3;
 
     $new_canvas = imagecreatetruecolor($new_w,$new_h);
+    imagecopyresampled($new_canvas,$canvas,0,0,0,0,$new_w,$new_h,$canvas_w,$canvas_h);
+
+    imagejpeg($new_canvas,"123.jpg");
+    imagepng($new_canvas,"123.png");
+    imagegif($new_canvas,"123.gif");
 
