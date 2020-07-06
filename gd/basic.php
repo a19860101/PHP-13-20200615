c1431739f66b1f423f2859310ea9f427d20cb666
Zac <a19860101@gmail.com>
2020-07-06 19:35:20 +0800

    20200706

---
+++ gd/basic.php
@@ -0,0 +1,15 @@
+<?php
+    $width = 800;
+    $height = 600;
+    $canvas = imagecreatetruecolor($width,$height);
+
+    $red = imagecolorallocate($canvas,255,0,0);
+    $green = imagecolorallocate($canvas,0,255,0);
+    $blue = imagecolorallocate($canvas,0,0,255);
+    $black = imagecolorallocate($canvas,0,0,0);
+    $white = imagecolorallocate($canvas,255,255,255);
+
+    imagefill($canvas,0,0,$white);
+
+    header("content-type:image/jpeg");
+    imagejpeg($canvas);
