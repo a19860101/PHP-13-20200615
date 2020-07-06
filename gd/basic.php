feabdfcac85af3fdd9dd10be1340b4735e3e65f6
Zac <a19860101@gmail.com>
2020-07-06 19:54:33 +0800

    20200706

--- gd/basic.php
+++ gd/basic.php
@@ -12,10 +12,15 @@
     imagefill($canvas,0,0,$white);
     imageline($canvas,0,0,800,600,$red);
     imageline($canvas,0,600,800,0,$blue);
+    imagestring($canvas,5,400,300,"hello",$red);
 
     header("content-type:image/png");
-    imagepng($canvas,"test.png");
-    header("content-type:image/gif");
-    imagegif($canvas,"test.gif");
-    header("content-type:image/jpeg");
-    imagejpeg($canvas,"test.jpeg");
+    imagepng($canvas);
+    
+
+    // header("content-type:image/png");
+    // imagepng($canvas,"test.png");
+    // header("content-type:image/gif");
+    // imagegif($canvas,"test.gif");
+    // header("content-type:image/jpeg");
+    // imagejpeg($canvas,"test.jpeg");
