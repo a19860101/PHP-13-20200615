9021238a88009f482c92648beffff9ae9c426f81
Zac <a19860101@gmail.com>
2020-07-08 20:31:39 +0800

    20200708

--- blog/template/nav.php
+++ blog/template/nav.php
@@ -6,7 +6,7 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">首頁</a>
+        <a class="nav-link" href="create_post.php">新增文章</a>
       </li>
     </ul>
   </div>
