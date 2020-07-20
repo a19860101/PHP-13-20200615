e5720279d464b215d3e0f200cd542d5616114085
Zac <a19860101@gmail.com>
2020-07-20 21:56:11 +0800

    20200720

--- blog/delete_post.php
+++ blog/delete_post.php
@@ -1,5 +1,10 @@
 <?php
     include("pdo.php");
     include("function/post.php");
+
+    if($_POST["path"] != "no-pic.png"){
+        unlink("images/".$_POST["path"]);
+    }
+
     delete($_POST["id"]);
     header("location:index.php");
--- blog/show_post.php
+++ blog/show_post.php
@@ -24,6 +24,7 @@
                 <a href="edit_post.php?id=<?php echo $row["id"];?>" class="btn btn-success">編輯</a>
                 <form action="delete_post.php" method="post" class="d-inline">
                     <input type="hidden" value="<?php echo $row["id"];?>" name="id">
+                    <input type="hidden" value="<?php echo $row["path"];?>" name="path">
                     <input type="submit" value="刪除" class="btn btn-danger" onclick="return confirm('確認刪除？')">
                 </form>
                 <?php }?>
