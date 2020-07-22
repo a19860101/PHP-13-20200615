404da10f29a4a76160cf93d9779861e9b664f0f1
Zac <a19860101@gmail.com>
2020-07-22 19:51:33 +0800

    20200722

--- blog/function/post.php
+++ blog/function/post.php
@@ -118,35 +118,35 @@
         $target = "images/{$name}";
         if($error == 0){
             if(move_uploaded_file($tmp_name,$target)){
+                imgResize($target,$type,$name);
                 return $name;
                 // echo "上傳成功";
                 // header("Refresh:1;url=index.php");
             }
         }
     }
-    function imgResize(){
-        $img = "images/01.jpg";
-        $canvas = imagecreatefromjpeg($img);
-    
+    function imgResize($target,$type,$name){
+        switch($type){
+            case "image/jpeg":
+                $canvas = imagecreatefromjpeg($target);
+                break;
+            case "image/png":
+                $canvas = imagecreatefrompng($target);
+                break;
+            case "image/gif":
+                $canvas = imagecreatefromgif($target);
+                break;
+        }
         $canvas_w = imagesx($canvas);
         $canvas_h = imagesy($canvas);
-        // echo $canvas_w,$canvas_h;
-    
-        // $new_w = $canvas_w / 3;
-        // $new_h = $canvas_h / 3;
-    
         $new_w = 600;
         $new_h = $canvas_h / $canvas_w * 600;
-    
-        // $new_h = 600;
-        // $new_w = $canvas_w / $canvas_h * 600;
-    
         $new_canvas = imagecreatetruecolor($new_w,$new_h);
         imagecopyresampled($new_canvas,$canvas,0,0,0,0,$new_w,$new_h,$canvas_w,$canvas_h);
     
-        imagejpeg($new_canvas,"images/456.jpg");
-        imagepng($new_canvas,"images/456.png");
-        imagegif($new_canvas,"images/456.gif");
+        imagejpeg($new_canvas,"thumbs/{$name}");
+        imagepng($new_canvas,"thumbs/{$name}");
+        imagegif($new_canvas,"thumbs/{$name}");
     
     
     }
